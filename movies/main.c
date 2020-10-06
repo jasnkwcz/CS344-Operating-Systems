cba7eda491c8d37a0305c0309788de21a02e9573
jasnkwcz <jasnkwcz@users.noreply.github.com>
2020-10-06 11:57:34 -0700

    Menu added to main file

--- movies/main.c
+++ movies/main.c
@@ -15,7 +15,36 @@ int main(int argc, char *argv[])
         printf("Example usage: ./Movies Movie_info1.txt\n");
         return EXIT_FAILURE;
     }
+
+    int response;
+    int year;
+    char *langptr = (char *)calloc(20, sizeof(char));
+
     struct Movie *list = processFile(argv[1]);
-    printMovieList(list);
+    do
+    {
+        printf("Menu:\n1. Show movies released in a specific year\n2. Show highest rated movie for each year\n3. Show all movies released in a specific language\n4. Exit program\nPlease select from the above options: ");
+        scanf("%d", &response);
+        switch (response)
+        {
+        case 1:
+            printf("Please enter a year: ");
+            scanf("%d", &year);
+            break;
+        case 2:
+
+            break;
+        case 3:
+            printf("Please select a language: ");
+            scanf("%s", langptr);
+            printf("Displaying all movies released in %s: \n", langptr);
+            break;
+        case 4:
+            printf("Goodbye\n");
+            break;
+        default:
+            printf("Invalid selection\n\n");
+        }
+    } while (response != 4);
     return EXIT_SUCCESS;
 }
