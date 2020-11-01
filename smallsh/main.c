ca7929ce1ca20ea207774329ca7dee948d9b6ef6
jasnkwcz <jasnkwcz@users.noreply.github.com>
2020-11-01 14:06:22 -0800

    added user input function

--- smallsh/main.c
+++ smallsh/main.c
@@ -21,30 +21,47 @@ struct Command
     char *inFile;
     char *outFile;
     bool *run_bg;
-}
+};
 
-int
-main(void)
+int getUserInput(char** line);
+struct Command *makeCmd();
+char **parseInput(char *line);
+
+int main(void)
 {
     //assign custom signal handlers for SIGINT and SIGSTP
 
+    //initialize varaibles
+    char *nline; //a command line
+    struct Command *newcmd = makeCmd(); //a command struct
+    char **nargv;
+
     while (true)
     {
-        //allocate memory for a new command struct including char arrays, etc.
-        //initialize variables
-        //get user input
-        getUserInput();
+        //get user input, store it in nline
+        if (getUserInput(&nline) == 1) {
+            continue;
+        };
         //parse input
+        nargv = parseInput(nline);
         //check if input is a comment or blank, if so, ignore
         //run the user command
         //clean up and destroy the command struct
-
         //vvvvONLY FOR DEBUGGING. REMOVE WHEN DONE **********************************************************************
         break;
         //^^^^ONLY FOR DEBUGGING. REMOVE WHEN DONE **********************************************************************
     }
     //clean up
-    return;
+    return 0;
+}
+
+/*
+makeCmd()
+    allocates memory for all command struct members and returns a pointer to the new command struct
+*/
+struct Command *makeCmd() {
+    struct Command *newCmd = (struct Command *)malloc(sizeof(struct Command));
+    return newCmd;
 }
 
 /*
@@ -61,12 +78,17 @@ getUserInput()
     prompts the user with ": " to enter a command 
     stores the user's input in a string specified in main
 */
-int getUserInput()
+int getUserInput(char** line)
 {
-    char prompt = ": ";
-    write(STDOUT_FILENO, prompt, strlen(prompt);
-    fflush();
-    return 0;
+    //print prompt to stdout
+    char*  prompt = ": ";
+    *line = (char *)calloc(MAXCHARS + 1, sizeof(char));
+    write(STDOUT_FILENO, prompt, strlen(prompt));
+    //read a line from stdout up to length of MAXCHARS
+    read(STDIN_FILENO, *line, MAXCHARS);
+    //resize the line argument based on the contents read from stdin, then copy the input into the line variable
+    *line = (char*)realloc(*line, strlen(*line));
+    return strlen(*line);
 }
 
 /*
@@ -74,6 +96,10 @@ parseInput()
     if the user input is blank or begins with "#", then input is ignored and returns 0
     else parse the input by splitting into tokens, storing in 
 */
+char **parseInput(char* line) {
+    
+}
+
 
 /*
 varExp()
@@ -101,7 +127,7 @@ Built-in command: exit
     runs in foreground only
     takes no arguments
     terminates all other currently running processes and jobs, then terminates the shell program
-/*
+*/
 
 /*
 Built-in command: status
